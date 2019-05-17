9b7f7237ec29a437d27097ef6f89fabe0d61c376
James Cape <james.cape@gmail.com>
2019-05-17 17:13:21 -0600

    Implements reroute back to login if user puts in invalid log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,21 @@
 class SessionsController < ApplicationController
 
   def login
-    
+
+  end
+
+  def create
+    @user = User.find_by(email: params[:email], password: params[:password])
+    require 'pry'; binding.pry
+
+    if @user == nil
+      redirect_to login_path
+      return
+    else
+
+    end
+
+    redirect_to users_path
   end
 
 
---
+++ spec/features/sessions/sessions_login_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "as a visitor" do
+  describe "when I visit /login" do
+    it "allows me to login" do
+      user_1 = User.create!(name: "John Smith", email: "john.smith@gmail.com", password: "airplay")
+
+      visit login_path
+
+      fill_in :email, with: user_1.email
+      fill_in :password, with: user_1.password
+
+      click_on "Log Me In!"
+
+      expect(current_path).to eq(users_path)
+      expect(page).to have_content("Welcome #{user_1.name}, you are logged in!")
+
+    end
+  end
+end
