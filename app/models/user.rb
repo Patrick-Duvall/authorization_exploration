b369097fefb0ae1041d0494d8dde9fd803027797
James Cape <james.cape@gmail.com>
2019-05-17 16:31:21 -0600

    Adds authenticate user spec and method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   validates_presence_of :name, :email, :password
+
+  def self.authenticate(email, password)
+    User.find_by(email: email, password: password)
+
+
+
+
+
+  end
 end
--- spec/models/users/user_spec.rb
+++ spec/models/users/user_spec.rb
@@ -6,4 +6,14 @@ describe User, type: :model do
     it {should validate_presence_of(:email)}
     it {should validate_presence_of(:password)}
   end
+
+  it "authenticates a user by email and password" do
+    user_1 = User.create!(name: "Joe", email: "joe@gmial.com", password: "elephant")
+
+    expect(User.authenticate(user_1.email, user_1.password)).to eq(user_1)
+    expect(User.authenticate("notjoe@gmail.com", user_1.password)).to eq(nil)
+    expect(User.authenticate(user_1.email, "notelephant")).to eq(nil)
+  end
+
+
 end
