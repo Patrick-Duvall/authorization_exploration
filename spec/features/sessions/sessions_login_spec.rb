372cac29980dbfe9c99f1e3debc072130a6f1668
James Cape <james.cape@gmail.com>
2019-05-17 17:35:16 -0600

    Tests invalid user log in is redirected back to login_path

--- spec/features/sessions/sessions_login_spec.rb
+++ spec/features/sessions/sessions_login_spec.rb
@@ -16,5 +16,18 @@ describe "as a visitor" do
       expect(page).to have_content("Welcome #{user_1.name}, you are logged in!")
       expect(page).to_not have_link("Log In")
     end
+
+    it "does not allow invalid user to log in" do
+      user_1 = User.create!(name: "John Smith", email: "john.smith@gmail.com", password: "airplay")
+
+      visit login_path
+
+      fill_in :email, with: "notjohn.smith@gmail.com"
+      fill_in :password, with: user_1.password
+
+      click_on "Log Me In!"
+
+      expect(current_path).to eq(login_path)
+    end
   end
 end
